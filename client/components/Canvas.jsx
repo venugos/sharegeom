2ac5f559803205d9f06d48a135d705d7c8fbb2fd
Neeharika Taneja <neeharika.taneja@autodesk.com>
2016-08-24 16:22:07 -0700

    some shapes

--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -9,7 +9,8 @@ var Canvas = () => (
   <div className="stage">
     <ReactKonva.Stage height={300} width={300}>
         <ReactKonva.Layer>
-          <ReactKonva.Rect x={100} y={100} width={50} height={50} fill="black" />
+          <ReactKonva.Rect x={100} y={100} width={50} height={50} stroke= 'black' opacity={0.5} draggable="true"/>
+          <ReactKonva.Circle x={20} y={20} radius={10}  stroke='black' draggable="true" opacity={0.5}/>
         </ReactKonva.Layer>
       </ReactKonva.Stage>
   </div>
