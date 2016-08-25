138302a2315fa9cb0bc9b6aafd478f2a41e4a1ab
Jill Shay <jill.shay@autodesk.com>
2016-08-24 18:22:11 -0700

    Rendering shapes passed from App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,7 +4,15 @@ var ReactDOM = require('react-dom');
 var Canvas = require('./Canvas.jsx');
 var Nav = require('./Nav.jsx');
 var className = require('classnames');
+var ReactKonva = require('react-konva');
 
+
+
+var shapes = [
+<ReactKonva.Rect x={100} y={100} width={50} height={50} stroke= 'black' opacity={0.5} draggable="true"/>,
+<ReactKonva.Circle x={20} y={20} radius={10}  stroke='black' draggable="true" opacity={0.5}/>,
+
+]
 
 var App = () => (
   <div className="app">
@@ -12,7 +20,7 @@ var App = () => (
       <div className="logo"></div>
       <h1 className="title">ShareGeom</h1>
         <Nav />
-      <Canvas />
+      <Canvas shapes={shapes}/>
     </div>
   </div>
 );
--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -1,18 +1,27 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactKonva = require('react-konva');
-
 var className = require('classnames');
 
+var onLayerClick = function (props) {
+  console.log("Clicked!!")
+}
 
-var Canvas = () => (
-  <div className="stage">
-    <ReactKonva.Stage height={300} width={300}>
-        <ReactKonva.Layer>
-          <ReactKonva.Rect x={100} y={100} width={50} height={50} fill="black" />
-        </ReactKonva.Layer>
-      </ReactKonva.Stage>
-  </div>
-);
+
+var Canvas = (props) => {
+  console.log(props);
+    return  (
+    <div className="stage">
+      <ReactKonva.Stage height={600} width={600}>
+          <ReactKonva.Layer >
+            { props.shapes } 
+          </ReactKonva.Layer>
+        </ReactKonva.Stage>
+    </div>
+  );
+}
+
+
 
 module.exports = Canvas;
+
