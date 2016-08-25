8211a1b38d96073bd74227e4a8140fe77d6ed245
Sarat Venugopal <saratv.dev@gmail.com>
2016-08-25 13:49:31 -0700

    Merging skeleton changes into master

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,9 +8,7 @@ var className = require('classnames');
 var App = () => (
   <div className="app">
     <div className="outer">
-      <div className="logo"></div>
       <h1 className="title">ShareGeom</h1>
-      <Nav />
       <Canvas/>
     </div>
   </div>
