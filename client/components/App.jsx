1ff6208d154d9c37d55ed071e27f2e40452916e1
Sarat Venugopal <saratv.dev@gmail.com>
2016-08-25 13:54:08 -0700

    Make skeleton up to date with master

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,20 +4,12 @@ var ReactDOM = require('react-dom');
 var Canvas = require('./Canvas.jsx');
 var Nav = require('./Nav.jsx');
 var className = require('classnames');
-var ReactKonva = require('react-konva');
-
-var shapes = [
-<ReactKonva.Rect x={25} y={50} width={50} height={50} stroke='black' draggable="true" strokeWidth={5} opacity={0.5}/>,
-<ReactKonva.Circle x={50} y={150} radius={30}  stroke='blue' draggable="true" strokeWidth={5} opacity={0.5}/>,
-]
-
-
 
 var App = () => (
   <div className="app">
     <div className="outer">
       <h1 className="title">ShareGeom</h1>
-      <Canvas shapes={shapes}/>
+      <Canvas/>
     </div>
   </div>
 );
--- server/server.js
+++ server/server.js
@@ -9,18 +9,18 @@ var util = require('util');
 var Duplex = require('stream').Duplex;
 
 // Start ShareDB
-var share = ShareDB({db: new ShareDBMingoMemory()});
+var share = ShareDB({ db: new ShareDBMingoMemory() });
 
 // Create a WebSocket server
 var app = connect();
 app.use(serveStatic('client/public'));
 var server = http.createServer(app);
-var wss = new WebSocket.Server({server: server});
+var wss = new WebSocket.Server({ server: server });
 server.listen(8080);
 console.log("Listening on http://localhost:8080");
 
 // Connect any incoming WebSocket connection with ShareDB
-wss.on('connection', function(ws, req) {
+wss.on('connection', function (ws, req) {
   var stream = new WebSocketJSONStream(ws);
   share.listen(stream);
 });
@@ -36,16 +36,39 @@ wss.on('connection', function(ws, req) {
 // }
 
 var connection = share.connect();
-connection.createFetchQuery('players', {}, {}, function(err, results) {
+// connection.createFetchQuery('shapes', {}, {}, function(err, results) {
+//   if (err) { throw err; }
+
+//   if (results.length === 0) {
+//     var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
+//                  "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+
+//     names.forEach(function(name, index) {
+//       var doc = connection.get('shapes', ''+index);
+//       var data = {name: name, score: Math.floor(Math.random() * 10) * 5};
+//       doc.create(data);
+//     });
+//   }
+// });
+
+connection.createFetchQuery('shapes', {}, {}, function (err, results) {
   if (err) { throw err; }
 
   if (results.length === 0) {
-    var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                 "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+    var shapes = [
+      { "attrs": { "x": 20, "y": 20, "radius": 10, "stroke": "black", "opacity": 0.5, "id": "06cd3a10-6a80-11e6-a419-2d4513a4e8d9", "draggable": true }, "className": "Circle" },
+      { "attrs": { "x": 100, "y": 100, "width": 50, "height": 70, "stroke": "black", "opacity": 0.5, "id": "06cd6120-6a80-11e6-a419-2d4513a4e8d9", "draggable": true }, "className": "Rect" }
+    ];
 
-    names.forEach(function(name, index) {
-      var doc = connection.get('players', ''+index);
-      var data = {name: name, score: Math.floor(Math.random() * 10) * 5};
+    shapes.forEach(function (shape, index) {
+      var doc = connection.get('shapes', shape.attrs.id);
+      // {
+      //   key: uuid,
+      //     attrs: props of shape,
+      //   className: type of shape  
+      // }
+      //console.log("Shape: ", shape);
+      var data = { key: shape.attrs.id, attrs: shape.attrs, className: shape.className };
       doc.create(data);
     });
   }
