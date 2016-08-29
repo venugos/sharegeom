75252422a6a1a70d596422bd2d253b4b7234169a
Sarat Venugopal <saratv.dev@gmail.com>
2016-08-28 20:08:48 -0700

    Revert WSS change from the last successful Heroku commit

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -7,12 +7,12 @@ var Init = require('./initdoc');
 /* global window, document; */
 
 // Open WebSocket connection to ShareDB server
-// let connection = new sharedb.Connection(
-//   new WebSocket('ws://' + window.location.host));
+let connection = new sharedb.Connection(
+  new WebSocket('ws://' + window.location.host));
 
 // Use this when committing to heroku and lose the commit
-let connection = new sharedb.Connection(
-new WebSocket('wss://' + window.location.host));
+// let connection = new sharedb.Connection(
+// new WebSocket('wss://' + window.location.host));
 
 // Expose to index.html
 window.renderApp = function () {
